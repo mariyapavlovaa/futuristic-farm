fae5476515b8c2e29bf9788f8e025bc52c0f1221
mariyapavlovaa <77243098+mariyapavlovaa@users.noreply.github.com>
2023-06-23 23:02:42 +0300

    change of the visualization of the farmer agent

--- src/main/java/com/uni/fmi/futuristicfarm/ai/gui/FarmerAgentGUI.java
+++ src/main/java/com/uni/fmi/futuristicfarm/ai/gui/FarmerAgentGUI.java
@@ -2,14 +2,8 @@ package com.uni.fmi.futuristicfarm.ai.gui;
 
 import com.uni.fmi.futuristicfarm.ai.agent.FarmerAgent;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import java.awt.BorderLayout;
-import java.awt.Container;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -23,6 +17,15 @@ public class FarmerAgentGUI extends JFrame {
     }
 
     private void initialize() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Farmer Agent");
+
         Container cp = getContentPane();
 
         JTextField resultNameTF = new JTextField(10);
@@ -31,23 +34,23 @@ public class FarmerAgentGUI extends JFrame {
         JLabel resultLabel = new JLabel("<html>Please, <br>" +
                 " click Show Result button <br>" +
                 "to see the found Illness of your crop.");
+        resultLabel.setForeground(Color.WHITE); // Set text color to white
 
         JButton resultButton = new JButton("Show Result");
+        resultButton.setForeground(Color.BLACK); // Set text color to black
 
         JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setBackground(new Color(0, 0, 128)); // RGB values for darker blue
         JPanel row = new JPanel();
+        row.setBackground(new Color(0, 0, 128)); // RGB values for darker blue
 
         row.add(resultLabel);
-        // row.add(resultNameTF);
         contentPane.add(row, BorderLayout.CENTER);
         contentPane.add(resultButton, BorderLayout.SOUTH);
 
         resultButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-//                String result = resultNameTF.getText();
-
                 String message = "Намерената болест е: " + myAgent.resultLabel;
                 JOptionPane.showMessageDialog(FarmerAgentGUI.this, message);
             }
